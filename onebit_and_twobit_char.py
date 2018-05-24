012312448fd2f49b0e1779edfc3a3103c8c69681
Calvin Ku <calvin.j.ku@googlemail.com>
2018-05-24 14:17:13 +0800

    added onebit and twobit chars

---
+++ onebit_and_twobit_char.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isOneBitCharacter(self, bits):
+        """
+        :type bits: List[int]
+        :rtype: bool
+        """
+        
+        i = 0
+        
+        while i < len(bits):
+            if i == len(bits) - 1:
+                return True
+            
+            if bits[i] == 1:
+                i += 2
+            else:
+                i += 1
+        
+        return False
+    
+        
